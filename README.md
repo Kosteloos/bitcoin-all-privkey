b3f2c7ba8e2f9ec9c99a59de7a43dd71d2e28c33
Kosteloos <51364589+Kosteloos@users.noreply.github.com>
2021-01-15 20:13:50 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+directory.io
+Install Go
+Install dependencies:
+
+$ go get github.com/btcsuite/btcd
+
+Run:
+
+$ go run directory.go
+
+By default, it listens on port 80. http://localhost:80
---
+++ directory.go
@@ -0,0 +1,219 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/big"
+	"net/http"
+    "io/ioutil"
+    "os"
+	"crypto/sha256"
+	"math/rand"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil"
+)
+
+const ResultsPerPage = 10000
+
+const PageTemplateHeader = `<html>
+<head>
+	<title>All bitcoin private keys</title>
+	<meta charset="utf8" />
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+	<style>
+		body{font-size: 9pt; font-family: 'Open Sans', sans-serif;}
+		a{text-decoration: none}
+		a:hover {text-decoration: underline}
+		.keys > span:hover { background: #f0f0f0; }
+		span:target { background: #ccffcc; }
+	</style>
+</head>
+<body>
+<h1>Bitcoin private key database</h1>
+<h2>Page %s out of %s</h2>
+<a href="/%s">previous</a> | <a href="/%s">next</a>
+<pre class="keys">
+<strong>Private Key</strong>                                            <strong>Address Uncompressed</strong>                               <strong>Address Compressed</strong>                 <strong>Balance</strong> 
+`
+
+const PageTemplateFooter = `</pre>
+<pre style="margin-top: 1em; font-size: 8pt">
+It took a lot of computing power to generate this database. Donations welcome: 1Bv8dN7pemC5N3urfMDdAFReibefrBqCaK
+</pre>
+<a href="/%s">previous</a> | <a href="/%s">next</a>
+</body>
+</html>`
+
+const KeyTemplate = `<span id="%s"><a href="/warning:understand-how-this-works!/%s">+</a> <span title="%s">%s </span> <a href="https://blockchain.info/address/%s">%34s</a> <a href="https://blockchain.info/address/%s">%34s</a>%10s %10s</span>
+`
+
+var (
+	// Total bitcoins
+	total = new(big.Int).SetBytes([]byte{
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFE,
+		0xBA, 0xAE, 0xDC, 0xE6, 0xAF, 0x48, 0xA0, 0x3B, 0xBF, 0xD2, 0x5E, 0x8C, 0xD0, 0x36, 0x41, 0x40,
+	})
+
+	// One
+	one = big.NewInt(1)
+
+	// Total pages
+	_pages = new(big.Int).Div(total, big.NewInt(ResultsPerPage))
+	pages  = _pages.Add(_pages, one)
+)
+
+type Key struct {
+	private      string
+	private2      string
+	number       string
+	compressed   string
+	uncompressed string
+	compressed2   string
+	uncompressed2 string
+}
+
+func getbalance(address string) string {
+  response, err := http.Get("https://blockchain.info/q/getreceivedbyaddress/" + address)
+  if err != nil {
+      fmt.Printf("%s", err)
+      os.Exit(1)
+  }
+  defer response.Body.Close()
+  contents, err := ioutil.ReadAll(response.Body)
+  if err != nil {
+    fmt.Printf("%s", err)
+    os.Exit(1)
+  }
+  return string(contents)
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func RandStringBytes(n int) string {
+    b := make([]byte, n)
+    for i := range b {
+        b[i] = letterBytes[rand.Intn(len(letterBytes))]
+    }
+    return string(b)
+}
+func compute(count *big.Int) (keys [ResultsPerPage]Key, length int) {
+	var padded [32]byte
+
+	var i int
+	for i = 0; i < ResultsPerPage; i++ {
+		// Increment our counter
+		count.Add(count, one)
+
+		// Check to make sure we're not out of range
+		if count.Cmp(total) > 0 {
+			break
+		}
+
+		// Copy count value's bytes to padded slice
+		copy(padded[32-len(count.Bytes()):], count.Bytes())
+
+		// Get private and public keys
+		privKey, public := btcec.PrivKeyFromBytes(btcec.S256(), padded[:])
+		s := RandStringBytes(20)
+        h := sha256.New()
+	    h.Write([]byte(s))
+        privKey2, public2 := btcec.PrivKeyFromBytes(btcec.S256(), h.Sum(nil))
+
+		// Get compressed and uncompressed addresses for public key
+		caddr, _ := btcutil.NewAddressPubKey(public.SerializeCompressed(), &chaincfg.MainNetParams)
+		uaddr, _ := btcutil.NewAddressPubKey(public.SerializeUncompressed(), &chaincfg.MainNetParams)
+        caddr2, _ := btcutil.NewAddressPubKey(public2.SerializeCompressed(), &chaincfg.MainNetParams)
+		uaddr2, _ := btcutil.NewAddressPubKey(public2.SerializeUncompressed(), &chaincfg.MainNetParams)
+
+		// Encode addresses
+		wif, _ := btcutil.NewWIF(privKey, &chaincfg.MainNetParams, false)
+        wif2, _ := btcutil.NewWIF(privKey2, &chaincfg.MainNetParams, false)
+		keys[i].private = wif.String()
+        keys[i].private2 = wif2.String()
+		keys[i].number = count.String()
+		keys[i].compressed = caddr.EncodeAddress()
+		keys[i].uncompressed = uaddr.EncodeAddress()
+        keys[i].compressed2 = caddr2.EncodeAddress()
+		keys[i].uncompressed2 = uaddr2.EncodeAddress()
+	}
+	return keys, i
+}
+
+func PageRequest(w http.ResponseWriter, r *http.Request) {
+	// Default page is page 1
+	if len(r.URL.Path) <= 1 {
+		r.URL.Path = "/1"
+	}
+
+	// Convert page number to bignum
+	page, success := new(big.Int).SetString(r.URL.Path[1:], 0)
+	if !success {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// Make sure page number cannot be negative or 0
+	page.Abs(page)
+	if page.Cmp(one) == -1 {
+		page.SetInt64(1)
+	}
+
+	// Make sure we're not above page count
+	if page.Cmp(pages) > 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	// Get next and previous page numbers
+	previous := new(big.Int).Sub(page, one)
+	next := new(big.Int).Add(page, one)
+
+	// Calculate our starting key from page number
+	start := new(big.Int).Mul(previous, big.NewInt(ResultsPerPage))
+
+	// Send page header
+	fmt.Fprintf(w, PageTemplateHeader, page, pages, previous, next)
+
+	// Send keys
+	keys, length := compute(start)
+	for i := 0; i < length; i++ {
+		key := keys[i]
+		balanceu := getbalance(key.uncompressed2)
+		balancec := getbalance(key.compressed2)
+		if balanceu > "0" {
+		  fmt.Fprintf(w, KeyTemplate, key.private2, key.private2, key.number, key.private2, key.uncompressed2, key.uncompressed2, key.compressed2, key.compressed2, balanceu, balancec)
+		} else if balancec > "0" {
+		  fmt.Fprintf(w, KeyTemplate, key.private2, key.private2, key.number, key.private2, key.uncompressed2, key.uncompressed2, key.compressed2, key.compressed2, balanceu, balancec)
+	    }
+    }
+
+	// Send page footer
+	fmt.Fprintf(w, PageTemplateFooter, previous, next)
+}
+
+func RedirectRequest(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Path[36:]
+
+	wif, err := btcutil.DecodeWIF(key)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	page, _ := new(big.Int).DivMod(new(big.Int).SetBytes(wif.PrivKey.D.Bytes()), big.NewInt(ResultsPerPage), big.NewInt(ResultsPerPage))
+	page.Add(page, one)
+
+	fragment, _ := btcutil.NewWIF(wif.PrivKey, &chaincfg.MainNetParams, false)
+
+	http.Redirect(w, r, "/"+page.String()+"#"+fragment.String(), http.StatusTemporaryRedirect)
+}
+
+func main() {
+	http.HandleFunc("/", PageRequest)
+	http.HandleFunc("/warning:understand-how-this-works!/", RedirectRequest)
+
+	log.Println("Listening")
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
